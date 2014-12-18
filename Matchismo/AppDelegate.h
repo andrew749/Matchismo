5af64661e90fa0c802610c5151378ed9a3e8d986
Andrew Codispoti <andrewcodispoti@Andrews-Mac-mini.local>
2014-12-18 00:09:13 -0500

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Andrew Codispoti on 2014-12-18.
+//  Copyright (c) 2014 Andrew Codispoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Andrew Codispoti on 2014-12-18.
+//  Copyright (c) 2014 Andrew Codispoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
